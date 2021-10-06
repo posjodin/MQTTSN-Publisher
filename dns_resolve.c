c9319a987fd5ba96419e19eb618e8b6fce14f4c0
Peter Sjödin <psj@kth.se>
2021-10-06 19:27:57 +0200

    Comment DNS resolve

--- dns_resolve.c
+++ dns_resolve.c
@@ -11,6 +11,7 @@
 #include "net/sock/dns.h"
 #endif
 #include "xtimer.h"
+#include "hashes.h"
 
 #ifdef APP_WATCHDOG
 #include "app_watchdog.h"
@@ -30,24 +31,52 @@ struct dns_cache {
         UNUSED,
         RESOLVED
     } state;
-    
 };
 typedef struct dns_cache dns_cache_t;
 
 static dns_cache_t dns_resolve_cache[DNS_CACHE_SIZE];
 
 static EEMEM dns_cache_t ee_dns_resolve_cache[DNS_CACHE_SIZE];
+static EEMEM uint32_t ee_hash;
 
-void dns_resolve_init(void) {
-    dns_cache_t *dc;
+/*
+ * Read DNS cache from EEPROM. Use a hash in EEPROM to verify that the info in the
+ * EEPROM is valid. Read info, compute hash, and compare with
+ * hash in EEPROM. Return non-zero if hashes match (ie info in EEPROM
+ * is valid).
+ */
+static int read_eeprom(void) {
     eeprom_read_block(&dns_resolve_cache, &ee_dns_resolve_cache, sizeof(dns_resolve_cache));
-    for (dc = &dns_resolve_cache[0]; dc <= &dns_resolve_cache[DNS_CACHE_SIZE-1]; dc++) {
-        if (dc->state != RESOLVED)
-            dc->state = UNUSED;
-    }
-    eeprom_update_block(&dns_resolve_cache, &ee_dns_resolve_cache, sizeof(dns_resolve_cache));    
+    uint32_t ehash = eeprom_read_dword(&ee_hash);
+    uint32_t hash = dek_hash((uint8_t *) &dns_resolve_cache, sizeof(dns_resolve_cache));
+    return ehash == hash;
 }
 
+/*
+ * Update cache and its hash in EEPROM
+ */
+static void update_eeprom(void) {
+    eeprom_update_block(&dns_resolve_cache, &ee_dns_resolve_cache, sizeof(dns_resolve_cache));
+    uint32_t hash = dek_hash((uint8_t *) &dns_resolve_cache, sizeof(dns_resolve_cache));
+    eeprom_update_dword(&ee_hash, hash);
+}
+
+/*
+ * Init cache. Read from EEPROM. If EEPROM info is not valid, mark all places as unused.
+ * Cache will be written back to EEPROM when it is updated.
+ */
+void dns_resolve_init(void) {
+    dns_cache_t *dc;
+    if (read_eeprom() == 0) {
+        for (dc = &dns_resolve_cache[0]; dc <= &dns_resolve_cache[DNS_CACHE_SIZE-1]; dc++) {
+            dc->state = UNUSED;
+        }
+    }
+}
+
+/*
+ * Is host in cache?
+ */
 static dns_cache_t *cache_lookup(char *host) {
     dns_cache_t *dc;
     for (dc = &dns_resolve_cache[0]; dc <= &dns_resolve_cache[DNS_CACHE_SIZE-1]; dc++) {
@@ -57,6 +86,9 @@ static dns_cache_t *cache_lookup(char *host) {
     return NULL;
 }
 
+/*
+ * Find an unused cache entry. If none exists, expire oldest entry and use that.
+ */
 static dns_cache_t *cache_alloc(void) {
     dns_cache_t *dc, *oldest;
     oldest = NULL;
@@ -70,6 +102,10 @@ static dns_cache_t *cache_alloc(void) {
     return oldest;
 }
 
+/*
+ * Update cache info after lookup, and update EEPROM as well.
+ * TODO: use epoch time, not local timestamp.
+ */
 static void cache_update(char *host, ipv6_addr_t *result) {
     dns_cache_t *cache_entry = cache_lookup(host);
     if (cache_entry == NULL)
@@ -79,17 +115,20 @@ static void cache_update(char *host, ipv6_addr_t *result) {
         cache_entry->ipv6addr = *result;
         cache_entry->state = RESOLVED;
         cache_entry->time_usec = xtimer_now_usec();
-        eeprom_update_block(&dns_resolve_cache, &ee_dns_resolve_cache, sizeof(dns_resolve_cache));    
+        update_eeprom();
     }
 }
 
+/*
+ * Resolve host name to IP address, and update cache if successful.
+ */
 static int _resolve_inetaddr(char *host, ipv6_addr_t *result) {
-#if defined(MODULE_SOCK_DNS) || defined(MODULE_SIM7020_SOCK_DNS) 
+#if defined(MODULE_SOCK_DNS) || defined(MODULE_SIM7020_SOCK_DNS)
 #ifdef DNS_RESOLVER
     sock_dns_server.family = AF_INET6;
-    sock_dns_server.port = 53;    
+    sock_dns_server.port = 53;
     if (ipv6_addr_from_str((ipv6_addr_t *)&sock_dns_server.addr.ipv6, DNS_RESOLVER) == NULL) {
-         printf("Bad resolver address %s\n", DNS_RESOLVER);
+         printf("Bad resolver %s\n", DNS_RESOLVER);
          return -1;
     }
 #endif /* DNS_RESOLVER */
@@ -107,9 +146,15 @@ static int _resolve_inetaddr(char *host, ipv6_addr_t *result) {
     return res;
 #else
     return -1;
-#endif    
+#endif
 }
 
+/*
+ * Resolve host. If IPv6 address string, convert to IPv6
+address and return. Otherwise, check if host is in the
+cache already. If so, return address in cache. If it is
+not in cache, make a DNS query.
+*/
 int dns_resolve_inetaddr(char *host, ipv6_addr_t *result) {
     /* Is host a v6 address? */
     if (ipv6_addr_from_str(result, host) != NULL) {
@@ -125,6 +170,12 @@ int dns_resolve_inetaddr(char *host, ipv6_addr_t *result) {
     return _resolve_inetaddr(host, result);
 }
 
+/*
+ * Cache refresh. Check if info in cache is old and should
+ * be updated.
+ * We do not want to be stuck in here too long, so only
+ * update one entry at a time.
+ */
 void dns_resolve_refresh(void) {
     dns_cache_t *dc;
     for (dc = &dns_resolve_cache[0]; dc <= &dns_resolve_cache[DNS_CACHE_SIZE-1]; dc++) {
